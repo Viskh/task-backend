780b01e70ed939b1418ba4903c547f3830de7f0e
Ramzan <ramzan4ik95@mail.ru>
2022-04-06 09:44:28 +0300

    heroku

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(require("./routes"));
 
 const connected = async () => {
